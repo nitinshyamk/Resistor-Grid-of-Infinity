8af07a8db3224f091c2ca1c865178ae91708cef6
Nitin Shyamkumar <nitinshyamk@gmail.com>
2014-03-26 19:27:42 -0400

    Updated GUI
    Now has colors (actually makes a difference when attempting to compare
    potentials, easier than grayscale)
    GUI also works from command line now in addition to Grid.java

--- JavaResistorGrid/src/GUI.java
+++ JavaResistorGrid/src/GUI.java
@@ -10,18 +10,41 @@ import javax.swing.*;
 
  public class GUI{
 	 private JFrame DisplayGrid = new JFrame();
-	 private static final int WIDTH = 20;
-	 private static final int HEIGHT = WIDTH;
+	 private static int WIDTH;
+	 private static int HEIGHT = WIDTH;
+	 private static int CELL_SIZE;
 	 private static final Color INIT_COLOR = Color.black;
 	 private Grid g;
 	 
+	 public static void main (String args[]) {
+		 String info = "GUI Creates a visual simulation of a resistor grid (a square) with size s:"
+		 		+ "Takes two arguments in the form: GUI <size (int) > <cell size> ";
+		 int s = 0,c = 0;
+		 if (args.length != 0){
+		 try{
+			 s = Integer.parseInt(args[0]);
+			 c = Integer.parseInt(args[1]);
+		 }
+		 catch (Exception e){
+			 e.printStackTrace();
+			 System.out.println(info);
+		 }
+		 }
+		 else{s = 20; c  = 4;}
+		 WIDTH = s; HEIGHT = s;
+		 CELL_SIZE = c;
+		 System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+		 	//line above to address bug in javax.swing package
+		 GUI grid = new GUI(); 
+	 }
+	 
 	 GUI(){
 		g = new Grid(WIDTH,HEIGHT);
-		g.setSource(8, 8);
-		g.setDrain(7, 7);
+		g.setSource(WIDTH/2,HEIGHT/2);
+		g.setDrain(WIDTH/2 + 1, HEIGHT/2 + 1);
 		g.Equalize();
 		System.out.println("Initializing Graphics");
-		DisplayGrid.setSize(WIDTH*10, HEIGHT*10);
+		DisplayGrid.setSize(WIDTH*CELL_SIZE, HEIGHT*CELL_SIZE);
 		DisplayGrid.setTitle("Resistor Grid: Potential MAP");
 		DisplayGrid.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container displaypane = DisplayGrid.getContentPane();
@@ -43,16 +66,12 @@ import javax.swing.*;
 	 public class Cell extends JPanel{
 		 Cell(Position pos){
 			double percentage = g.getNode(pos.r, pos.c).getVoltage()/10.0;
-			percentage = percentage*255.0;
-			int colorval = (int)(Math.round(percentage));
-			this.setBackground(new Color(colorval, colorval, colorval));
+			percentage = percentage*0.8;
+			this.setBackground(Color.getHSBColor((float) percentage, 1, 1));
+			//this.setBackground(new Color(colorval, colorval, colorval));
 			this.setPreferredSize(new Dimension (2,2));
 		 }
-		 
 	 }
-	 public static void main (String args[]) {
-		 System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
-		 	//line above to address bug in javax.swing package
-		 GUI grid = new GUI(); 
-	 }
+	 
+
 }
---
+++ JavaResistorGrid/src/testGrid.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "This program executes the Java program Grid.java and sends the values to a text file labeled voltmap.txt"
+java Grid >voltmap.txt
